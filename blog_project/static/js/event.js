d73f7916e24f2b4895323d29f131422f51f7bf3c
JosueAcosta <josueacosta@Isidros-MackBookPro.local>
2017-08-30 14:39:56 -0400

    Modified file to clean up code

--- blog_project/static/js/event.js
+++ blog_project/static/js/event.js
@@ -7,19 +7,13 @@ $("body").on("click", "button.like_button", function(e){
 		var url = "/blog/like/" + id;
 
 		$.post(url,{"isLike": "1"}, function(data, status){
-			//console.log(data.replace(new RegExp("'", 'g'), '"'));
+			
 			var obj = data.replace(new RegExp("'", 'g'), '"');
 			var objp = JSON.parse(obj);
 			console.log(obj);
 			console.log(objp.likes);
 			$('#like-counter_' + id).html(objp.likes);
 			$('#dislike-counter_' + id).html(objp.dislikes);
-			//var obj = JSON.parse(data.replace(new RegExp("'", 'g'), '"'));
-			//console.log( JSON.stringify(data) + "\nStatus: " + status);
-			//console.log(obj.likes);
-
-			//alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-
 		});
 	}
 });
@@ -32,18 +26,13 @@ $("body").on("click", "button.dislike_button", function(e){
 		var url = "/blog/like/" + id;
 
 		$.post(url,{"isLike": "0"}, function(data, status){
-			//console.log(data.replace(new RegExp("'", 'g'), '"'));
+			
 			var obj = data.replace(new RegExp("'", 'g'), '"');
 			var objp = JSON.parse(obj);
 			console.log(obj);
 			console.log(objp.dislikes);
 			$('#like-counter_' + id).html(objp.likes);
 			$('#dislike-counter_' + id).html(objp.dislikes);
-			//var obj = JSON.parse(data.replace(new RegExp("'", 'g'), '"'));
-			//console.log(JSON.stringify(data) + "\nStatus: " + status);
-			//console.log(obj.dislikes);
-			//alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-
 		});
 	}
 });
